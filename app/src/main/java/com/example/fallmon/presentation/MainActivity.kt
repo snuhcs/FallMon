9dea629ed24fd3626c60b25617582c962e1868f9
SYSTEMERRORCODE0 <39697564+SYSTEMERRORCODE0@users.noreply.github.com>
2023-10-21 17:27:00 +0900

    Application sensor detection Finished (#6)
    * Feat: display accelerometer data on the screen
    
    * Sampling rate to 30Hz &
    
    Added array for collecting sensor data
    75 * 3 (currently)
    
    * Calculate Average, StandardDeviation in 75 * 3 sensor data.
    
    Looks ok but,
    Need to be test in real watch.
    
    * Calculate RootMeanSquare, MaxAmplitude, MinAmplitude in 75 * 3 sensor data.
    
    Looks ok but,
    Need to be test in real watch.
    
    * Calculate Median, NZC, Skewness, Kurtosis, Percentile1, Percentile3 in 75 * 3 sensor data.
    
    Looks ok but,
    Need to be test in real watch.
    
    * Move Math functions to com.example.fallmon.presentation.math.FallMonMath
    
    A new object file.
    
    * Added function for FrequencySpectrum
    
    library for FFT added
    
    added FreqAverage, FreqMedian
    
    * Fixed FFT error: Now output same fft result as np.fft.fft
    
    * Fixed FFT error: Now output same fft result as np.fft.fft
    
    * Revert "Fixed FFT error: Now output same fft result as np.fft.fft"
    
    This reverts commit 46da6fa281adf9af77573a82fcd7daa3b955a57a.
    
    * RE: Fixed FFT error: Now output same fft result as np.fft.fft
    
    * RE:RE: Fixed FFT error: Now output same fft result as np.fft.fft
    
    * Remove Useless data class (while modify FFT)
    
    * Fixed entropy, energy function.
    
    All 15 values now are identical to features in python.
    
    * Fixed entropy, energy function.
    
    All 15 values now are identical to features in python.
    
    ---------
    
    Co-authored-by: sangmin7b <sangmin7b@gmail.com>

--- app/src/main/java/com/example/fallmon/presentation/MainActivity.kt
+++ app/src/main/java/com/example/fallmon/presentation/MainActivity.kt
@@ -6,36 +6,209 @@
 
 package com.example.fallmon.presentation
 
+import android.annotation.SuppressLint
+import android.content.Context.*
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.example.fallmon.R
 import com.example.fallmon.presentation.theme.FallMonTheme
+import com.example.fallmon.presentation.math.FallMonMath as FMath
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : ComponentActivity(), SensorEventListener {
+
+    private val SAMPLING_RATE: Int = 30
+    private val WINDOW_SIZE: Int = 75
+    private val WINDOW_STRIDE: Int = 15
+
+    private lateinit var sensorManager: SensorManager
+    private var mAccelerometer: Sensor ?= null
+    private lateinit var text_square: TextView
+
+    // sensor_window, window_index : for sliding window
+    private var sensor_window = Array(WINDOW_SIZE) { Array(3) { 0.0f } }
+    private var sensor_window_transpose = Array(3) { Array(WINDOW_SIZE) { 0.0f } }
+    private var window_index: Int = 0
+
+    /* Constructor */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WearApp("Android")
+        setContentView(R.layout.activity_main)
+        text_square = findViewById(R.id.text_view1)
+        setUpSensor()
+    }
+
+    /* Set up sensor when the app starts */
+    private fun setUpSensor(){
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also{
+            sensorManager.registerListener(
+                this,
+                it,
+                1000000 / SAMPLING_RATE,
+                1000000 / SAMPLING_RATE
+            )
         }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            // Process accelerometer data
+            val xAcceleration = event.values[0]
+            val yAcceleration = event.values[1]
+            val zAcceleration = event.values[2]
+            //text_square.text = "${window_index}  \nx: ${xAcceleration}  \ny:  ${yAcceleration}  \nz: ${zAcceleration}"
+
+            sensor_window[window_index % WINDOW_SIZE][0] = xAcceleration
+            sensor_window[window_index % WINDOW_SIZE][1] = yAcceleration
+            sensor_window[window_index % WINDOW_SIZE][2] = zAcceleration
+
+            // for easy calculate features. Erase this if has efficient way
+            sensor_window_transpose[0][window_index % WINDOW_SIZE] = xAcceleration
+            sensor_window_transpose[1][window_index % WINDOW_SIZE] = yAcceleration
+            sensor_window_transpose[2][window_index % WINDOW_SIZE] = zAcceleration
+
+            window_index += 1
+
+            if(window_index % WINDOW_STRIDE == 0 && window_index >= WINDOW_SIZE) {
+                sensorWindowFulled()
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun sensorWindowFulled() {
+
+        sensor_window_transpose = Array(3) { Array(WINDOW_SIZE) { 0.0f } }
+
+        for(i: Int in 0 until 3)
+            for(j: Int in 0 until WINDOW_SIZE)
+                sensor_window_transpose[i][j] = (i+1)*(j+1).toFloat()
+
+        val xAverage = sensor_window_transpose[0].average().toFloat()
+        val yAverage = sensor_window_transpose[1].average().toFloat()
+        val zAverage = sensor_window_transpose[2].average().toFloat()
+
+        val xStandardDeviation = FMath.standardDeviation(sensor_window_transpose[0], xAverage)
+        val yStandardDeviation = FMath.standardDeviation(sensor_window_transpose[1], yAverage)
+        val zStandardDeviation = FMath.standardDeviation(sensor_window_transpose[2], zAverage)
+
+        val xRootMeanSquare = FMath.rootMeanSquare(sensor_window_transpose[0])
+        val yRootMeanSquare = FMath.rootMeanSquare(sensor_window_transpose[1])
+        val zRootMeanSquare = FMath.rootMeanSquare(sensor_window_transpose[2])
+
+        val xMaxAmplitude = FMath.maxAmplitude(sensor_window_transpose[0])
+        val yMaxAmplitude = FMath.maxAmplitude(sensor_window_transpose[1])
+        val zMaxAmplitude = FMath.maxAmplitude(sensor_window_transpose[2])
+
+        val xMinAmplitude = FMath.minAmplitude(sensor_window_transpose[0])
+        val yMinAmplitude = FMath.minAmplitude(sensor_window_transpose[1])
+        val zMinAmplitude = FMath.minAmplitude(sensor_window_transpose[2])
+
+        val xMedian = FMath.median(sensor_window_transpose[0])
+        val yMedian = FMath.median(sensor_window_transpose[1])
+        val zMedian = FMath.median(sensor_window_transpose[2])
+
+        val xNZC = FMath.nzc(sensor_window_transpose[0])
+        val yNZC = FMath.nzc(sensor_window_transpose[1])
+        val zNZC = FMath.nzc(sensor_window_transpose[2])
+
+        val xSkewness = FMath.skewness(sensor_window_transpose[0], xAverage, xStandardDeviation)
+        val ySkewness = FMath.skewness(sensor_window_transpose[1], yAverage, yStandardDeviation)
+        val zSkewness = FMath.skewness(sensor_window_transpose[2], zAverage, zStandardDeviation)
+
+        val xKurtosis = FMath.kurtosis(sensor_window_transpose[0], xAverage, xStandardDeviation)
+        val yKurtosis = FMath.kurtosis(sensor_window_transpose[1], yAverage, yStandardDeviation)
+        val zKurtosis = FMath.kurtosis(sensor_window_transpose[2], zAverage, zStandardDeviation)
+
+        val xPercentile1 = FMath.percentile1(sensor_window_transpose[0])
+        val yPercentile1 = FMath.percentile1(sensor_window_transpose[1])
+        val zPercentile1 = FMath.percentile1(sensor_window_transpose[2])
+
+        val xPercentile3 = FMath.percentile3(sensor_window_transpose[0])
+        val yPercentile3 = FMath.percentile3(sensor_window_transpose[1])
+        val zPercentile3 = FMath.percentile3(sensor_window_transpose[2])
+
+        val xFreq = FMath.frequencySpectrum(sensor_window_transpose[0])
+        val yFreq = FMath.frequencySpectrum(sensor_window_transpose[1])
+        val zFreq = FMath.frequencySpectrum(sensor_window_transpose[2])
+
+        val xFreqAverage = xFreq.average().toFloat()
+        val yFreqAverage = yFreq.average().toFloat()
+        val zFreqAverage = zFreq.average().toFloat()
+
+        val xFreqMedian = FMath.median(xFreq)
+        val yFreqMedian = FMath.median(yFreq)
+        val zFreqMedian = FMath.median(zFreq)
+
+        // below two functions would be fixed
+        val xEntropy = FMath.entropy(xFreq)
+        val yEntropy = FMath.entropy(yFreq)
+        val zEntropy = FMath.entropy(zFreq)
+
+        val xEnergy = FMath.energy(xFreq)
+        val yEnergy = FMath.energy(yFreq)
+        val zEnergy = FMath.energy(zFreq)
+
+        text_square.text = """${window_index}
+            |x: ${xAverage}, ${xStandardDeviation}, ${xRootMeanSquare}, ${xMaxAmplitude}, ${xMinAmplitude}, ${xMedian}, ${xNZC}, ${xSkewness}, ${xKurtosis}, ${xPercentile1}, ${xPercentile3}, ${xFreqAverage}, ${xFreqMedian}, ${xEntropy}, ${xEnergy}
+            |""".trimMargin()
+        /*
+        text_square.text = """${window_index}
+            |x: ${xAverage}, ${xStandardDeviation}, ${xRootMeanSquare}, ${xMaxAmplitude}, ${xMinAmplitude}, ${xMedian}, ${xNZC}, ${xSkewness}, ${xKurtosis}, ${xPercentile1}, ${xPercentile3}, ${xFreqAverage}, ${xFreqMedian}, ${xEntropy}, ${xEnergy}
+            |y: ${yAverage}, ${yStandardDeviation}, ${yRootMeanSquare}, ${yMaxAmplitude}, ${yMinAmplitude}, ${yMedian}, ${yNZC}, ${ySkewness}, ${yKurtosis}, ${yPercentile1}, ${yPercentile3}, ${yFreqAverage}, ${yFreqMedian}, ${yEntropy}, ${yEnergy}
+            |z: ${zAverage}, ${zStandardDeviation}, ${zRootMeanSquare}, ${zMaxAmplitude}, ${zMinAmplitude}, ${zMedian}, ${zNZC}, ${zSkewness}, ${zKurtosis}, ${zPercentile1}, ${zPercentile3}, ${zFreqAverage}, ${zFreqMedian}, ${zEntropy}, ${zEnergy}
+            |""".trimMargin()
+
+         */
+    }
+
+
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun SensorData(v1: Float, v2: Float, v3: Float) {
+    FallMonTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.sensor_data, v1.toString(), v2.toString(), v3.toString())
+            )
+        }
+    }
+}
+
+
+@Composable
+fun WearApp() {
     FallMonTheme {
         /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
          * version of LazyColumn for wear devices with some added features. For more information,
@@ -47,23 +220,13 @@ fun WearApp(greetingName: String) {
                 .background(MaterialTheme.colors.background),
             verticalArrangement = Arrangement.Center
         ) {
-            Greeting(greetingName = greetingName)
+            SensorData(0f, 0f , 0f)
         }
     }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
---
+++ app/src/main/java/com/example/fallmon/presentation/math/FallMonMath.kt
@@ -0,0 +1,125 @@
+package com.example.fallmon.presentation.math
+
+import org.jtransforms.fft.DoubleFFT_1D
+import kotlin.math.abs
+import kotlin.math.ln
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+
+
+object FallMonMath {
+    fun standardDeviation(array: Array<Float>, average: Float): Float {
+        var variance = 0.0f
+        for(f in array) variance += (f - average).pow(2)
+        return sqrt(variance / array.size)
+    }
+
+    fun rootMeanSquare(array: Array<Float>): Float {
+        var sumSquare = 0.0f
+        for(f in array) sumSquare += f*f
+        return sqrt(sumSquare / array.size)
+    }
+
+    fun maxAmplitude(array: Array<Float>): Float {
+        var maxAmp = 0.0f
+        for(f in array) maxAmp = max(maxAmp, abs(f))
+        return maxAmp
+    }
+
+    fun minAmplitude(array: Array<Float>): Float {
+        var minAmp: Float = Float.MAX_VALUE
+        for(f in array) minAmp = min(minAmp, abs(f))
+        return minAmp
+    }
+
+    fun median(array: Array<Float>): Float {
+        val sortedArray = array.sorted()
+        return sortedArray[array.size / 2]
+    }
+
+    // Simplified assume that WINDOW_SIZE = 75
+    fun percentile1(array: Array<Float>): Float {
+        val sortedArray = array.sorted()
+        return (sortedArray[18] + sortedArray[19]) / 2
+    }
+
+    // Simplified assume that WINDOW_SIZE = 75
+    fun percentile3(array: Array<Float>): Float {
+        val sortedArray = array.sorted()
+        return (sortedArray[55] + sortedArray[56]) / 2
+    }
+
+    fun nzc(array: Array<Float>): Float {
+        val signArray = Array(array.size) { 0.0f }
+        for(i: Int in array.indices)
+            if(array[i] > 0) signArray[i] = 1.0f
+            else if(array[i] < 0) signArray[i] = -1.0f
+        // else signArray[i] = 0.0f // don't have to modify
+
+        var sum = 0.0f
+        for(i: Int in 1 until array.size)
+            sum += abs(signArray[i] - signArray[i-1])
+        return sum
+    }
+
+    fun skewness(array: Array<Float>, average: Float, standardDeviation: Float): Float {
+        val sumCubedDiff = array.sumOf { (it - average).toDouble().pow(3.0) }
+        val skewness = (sumCubedDiff / array.size) / standardDeviation.toDouble().pow(3.0)
+        return skewness.toFloat()
+    }
+
+    fun kurtosis(array: Array<Float>, average: Float, standardDeviation: Float): Float {
+        val sumFourthPowerDiff = array.sumOf { (it - average).toDouble().pow(4.0) }
+        val skewness = (sumFourthPowerDiff / array.size) / standardDeviation.toDouble().pow(4.0) - 3.0
+        return skewness.toFloat()
+    }
+
+    fun fft(input: DoubleArray): DoubleArray {
+        val fft = DoubleFFT_1D(input.size.toLong() * 2)
+
+        // make double -> ( real double, imag double(=0) )
+        val result = DoubleArray(input.size * 2) { 0.0 }
+        for (i in input.indices) result[i * 2] = input[i]
+
+        fft.realForward(result)
+        return result
+    }
+
+    fun frequencySpectrum(array: Array<Float>): Array<Float> {
+        // FFT needs DoubleArray Type
+        val doubleArray = DoubleArray(array.size) { array[it].toDouble() }
+
+        // perform FFT
+        val fftResultDouble = fft(doubleArray)
+        val fftResult = Array(array.size * 2) { fftResultDouble[it].toFloat() }
+
+        // Calculate frequency spectrum
+        val freqSpec = fftResult
+            .drop(2)    //  exclude index 0
+
+        val result = Array(array.size - 1) { 0.0f }
+        for(i in 0 until freqSpec.size/2) {
+            result[i] = sqrt(freqSpec[i*2].pow(2) + freqSpec[i*2+1].pow(2))
+        }
+
+        // resize the result and return
+        return result
+    }
+
+    fun entropy(array: Array<Float>): Float {
+
+        val sum = array.sum()
+        if(sum == 0.0f) return 0.0f
+
+        return -array.sumOf { (it / sum) * ln((it / sum).toDouble()) }.toFloat()
+    }
+
+    fun energy(array: Array<Float>): Float {
+        var squareSum = 0.0f
+        for(f in array) squareSum += f*f
+        return squareSum / array.size
+    }
+}
