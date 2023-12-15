9669529cb182a132288a94212f0ee0b844ee4e9f
SYSTEMERRORCODE0 <39697564+SYSTEMERRORCODE0@users.noreply.github.com>
2023-12-15 15:02:04 +0900

    Application basic UI merge into application (#20)
    * Created UI functions: not very visible, for divide actions for each work.
    
    * !!! Major Error Found !!! Application 1021 debug (#9)
    
    * Modified not-added features after merging sensor_detection & porting tools
    
    * !!! Major Error detected !!!
    
    fixed freqMedian, freqEntropy, freqEnergy in MainActivity.kt
    
    * Cherry-picked modularized feature extraction
    
    * Cherry-picked modularized feature extraction
    
    * Revert "Cherry-picked modularized feature extraction"
    
    This reverts commit 76416a20c9fccc5f2b9e318a68e55cce3147273b.
    
    * Cherry-picked modularized feature extraction
    
    * Add ported ClassificationModel
    
    * New UI for detection. Not functionally run now.
    
    * New UI for detection. Not functionally run now.
    
    * ConfirmedActivity added, modified total activites
    
    * Cherry-picked Application send fallhistory
    
    * Align activity codes for visibility
    
    * Align activity codes for visibility
    
    * FallDatectionService : detect falls even in background.
    Move the main function for it from MainActivity to FallDetectionService
    
    * Solved duplicated detection
    
    * Solving the application stopped by itself
    
    ---------
    
    Co-authored-by: sangmin7b <sangmin7b@gmail.com>

--- app/src/main/java/com/example/fallmon/presentation/DetectedActivity.kt
+++ app/src/main/java/com/example/fallmon/presentation/DetectedActivity.kt
@@ -1,21 +1,57 @@
 package com.example.fallmon.presentation
 
 import android.app.Activity
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
+import android.media.AudioAttributes
+import android.media.RingtoneManager
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.IBinder
+import android.util.Log
 import android.view.Gravity
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fallmon.R
+import com.example.fallmon.presentation.retrofit.FallMonService
+import com.example.fallmon.presentation.retrofit.dto.FallHistoryDTO
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class DetectedActivity : ComponentActivity() {
 
+    /*
+     * for countdown automatic data sending
+     */
     private lateinit var countDownTimer: CountDownTimer
     private val totalTimeInMillis: Long = 20000  // 20 seconds
 
+    /*
+     * for data sending
+     */
+    private val BaseURL: String = "http://34.22.106.16:8080"
+    private val TestUserID: String = "234532"
+    private val FallHistoryURL: String = "$BaseURL/api/fall_history"
+    private val retrofit: Retrofit by lazy {
+       Retrofit.Builder()
+            .baseUrl(BaseURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    private lateinit var fall: FallHistory
+
+    /*
+     * put data sending was confirmed (and sended) to MainActivity & intent finish
+     */
     private val getActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if(it.resultCode == RESULT_OK) {
             val resultIntent = Intent()
@@ -25,9 +61,20 @@ class DetectedActivity : ComponentActivity() {
         }
     }
 
+    private var fallDetectionService: FallDetectionService? = null
+
+    /*
+     * Set layout, buttons, views
+     * get classification result from MainActivity
+     * Decide falltype by result and show them.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detected) // Set your layout here
+        setContentView(R.layout.activity_detected)
+        Log.d("Detected onCreate", "created")
+
+        val serviceIntent = Intent(this, FallDetectionService::class.java)
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
 
         val confirmButton: ImageButton = findViewById(R.id.activity_detected_Confirm)
         val disconfirmButton: ImageButton = findViewById(R.id.activity_detected_Disconfirm)
@@ -36,26 +83,45 @@ class DetectedActivity : ComponentActivity() {
 
         val classificationResult = intent.getDoubleArrayExtra("classificationResult")
 
-        var fallType = "단순 낙상"
+        var fallType = FallType.FALL    // default : simple fall
 
         fallType = when(classificationResult?.max()) {
-            classificationResult?.get(0) -> "쓰러짐 (Drop attack)"
-            //classificationResult?.get(1) -> "비낙상 (Nonfall)"
-            classificationResult?.get(2) -> "미끄러짐 (Slipping)"
-            classificationResult?.get(3) -> "넘어짐 (Stand push)"
-            classificationResult?.get(4) -> "헛디딤 (Sunken floor)"
-            else -> "비낙상 (Nonfall)"
+            classificationResult?.get(0) -> FallType.DROP_ATTACK
+            //classificationResult?.get(1) -> FallType.NON_FALL
+            classificationResult?.get(2) -> FallType.SLIPPING
+            classificationResult?.get(3) -> FallType.STAND_PUSH
+            classificationResult?.get(4) -> FallType.SUNKEN_FLOOR
+            else -> FallType.NON_FALL
         }
 
         fallText.text = "낙상 감지!!!"
         fallText.gravity = Gravity.CENTER
-        fallTypeText.text = "$fallType"
+        fallTypeText.text = fallType.strFall
         fallTypeText.gravity = Gravity.CENTER
+
+        fall = FallHistory(TestUserID, fallType, Date())
+
+        /*
+         * alarm even if the volume is 0 in watch setting.
+         */
+        val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ALARM)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        val ringtone = RingtoneManager.getRingtone(applicationContext, alarmSound)
+        ringtone.audioAttributes = audioAttributes
+        ringtone.play()
 
         countDown()
 
+        /*
+         * confirm button : confirm the fall and send data to server
+         * disconfirm button : disconfirm the fall, not send data to server
+         */
         confirmButton.setOnClickListener {
-            confirmed()
+            confirmed(fall)
         }
 
         disconfirmButton.setOnClickListener {
@@ -66,6 +132,22 @@ class DetectedActivity : ComponentActivity() {
         }
     }
 
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName?, service: IBinder?) {
+            val binder = service as FallDetectionService.LocalBinder
+            fallDetectionService = binder.getService()
+            fallDetectionService?.notifyActivityCreated()
+        }
+
+        override fun onServiceDisconnected(className: ComponentName?) {
+            fallDetectionService = null
+        }
+    }
+
+    /*
+     * countdown automatic data sending
+     * remaining second is continuously put in UI
+     */
     private fun countDown() {
         val timerText: TextView = findViewById(R.id.activity_detected_TimerText)
         countDownTimer = object : CountDownTimer(totalTimeInMillis, 1000) {
@@ -76,20 +158,59 @@ class DetectedActivity : ComponentActivity() {
             }
 
             override fun onFinish() {
-                confirmed()
+                confirmed(fall)
             }
         }
 
         countDownTimer.start()
     }
 
-    private fun confirmed() {
+    /*
+     * request server to put fall data
+     */
+    private fun request(fallHistory: FallHistory){
+        try{
+            val createdStr = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(fallHistory.createdAt)
+            Log.d("request", "createdStr: ${createdStr}")
+            val server = retrofit.create(FallMonService::class.java)
+            server.createFallHistory(fallHistory.id, createdStr, fallHistory.fallType.strFall).enqueue(object : Callback<FallHistoryDTO>{
+                override fun onFailure(call: Call<FallHistoryDTO>, t: Throwable) {
+                    Log.e("Retrofit", t.toString())
+                }
+
+                override fun onResponse(
+                    call: Call<FallHistoryDTO>,
+                    response: Response<FallHistoryDTO>
+                ) {
+                    Log.d("Retrofit", "success")
+                    val id = response.body()?.id
+                    val createdAt = response.body()?.createdAt
+                    val fallType = response.body()?.fallType
+                }
+            })
+        }catch(e:Exception){
+            Log.e("Retrofit", e.toString())
+        }
+
+    }
+
+    /*
+     * run when that fall is confirmed
+     * request data sending, then intent ConfirmedActivity
+     */
+    private fun confirmed(fall: FallHistory) {
+        Log.d("Detected confirmed", "confirmed")
+        request(fall)
+        Log.d("Detected confirmed", "request")
         val intent = Intent(this, ConfirmedActivity::class.java)
         getActivityResult.launch(intent)
     }
 
     override fun onDestroy() {
+        Log.d("DetectedActivity", "Destroyed")
         super.onDestroy()
+        fallDetectionService?.notifyActivityFinished()
         countDownTimer.cancel()
+        unbindService(serviceConnection)
     }
 }
---
+++ app/src/main/java/com/example/fallmon/presentation/retrofit/FallMonService.kt
@@ -0,0 +1,16 @@
+package com.example.fallmon.presentation.retrofit
+
+import com.example.fallmon.presentation.retrofit.dto.FallHistoryDTO
+import retrofit2.Call
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+public interface FallMonService{
+    @POST("/api/fall_history")
+    fun createFallHistory(
+        @Query("user_id") id: String,
+        @Query("created_at") createdAt: String,
+        @Query("fall_type") fallType: String
+    ) : Call<FallHistoryDTO>
+
+}
